71d730ad7b6711943f39504572d16c4eff2ed5fe
İsmail Özer <220202093@kocaeli.edu.tr>
2026-08-07 00:52:55 +0300

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const openapi = require('./openapi.json');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapi));
 
 // Geçici, bellek içi görev listesi
 const tasks = [
