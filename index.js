e280c451042d58dc70869ab224f22cce1366e1d6
İsmail Özer <220202093@kocaeli.edu.tr>
2026-08-07 03:18:51 +0300

    Stage 0: create SQLite database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const openapi = require('./openapi.json');
+const db = require('./database');
 
 const app = express();
 const port = 3000;
